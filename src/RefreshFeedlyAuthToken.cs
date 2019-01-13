5f2e4ca32f831b4f4ec52c8d03c691469bab6a3f
Sean Killeen <SeanKilleen@gmail.com>
2019-01-12 19:46:23 -0500

    add environment variable concepts

--- src/RefreshFeedlyAuthToken.cs
+++ src/RefreshFeedlyAuthToken.cs
@@ -7,10 +7,16 @@ namespace FeedlyOpmlExport.Functions
 {
     public static class RefreshFeedlyAuthToken
     {
+        // These will be read from a settings file, or environment variables, which in production will point to the key vault.
+        public static string userId = System.Environment.GetEnvironmentVariable("feedly-user-id");
+        public static string accessToken = System.Environment.GetEnvironmentVariable("feedly-access-token");
+        public static string refreshToken = System.Environment.GetEnvironmentVariable("feedly-refresh-token");
+
         [FunctionName("RefreshFeedlyAuthToken")]
         public static void Run([TimerTrigger("0 0 */6 * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"UserId: {userId}, Access Token: {accessToken}, Refresh token: {refreshToken}");
         }
     }
 }
