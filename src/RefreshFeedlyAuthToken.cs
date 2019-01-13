d8a5a91f1a49ddc26d9236667c052c208fc087b7
Sean Killeen <SeanKilleen@gmail.com>
2019-01-13 16:55:50 -0500

    more cleanup

--- src/RefreshFeedlyAuthToken.cs
+++ src/RefreshFeedlyAuthToken.cs
@@ -11,16 +11,18 @@ using Newtonsoft.Json;
 
 namespace FeedlyOpmlExport.Functions
 {
+    // ReSharper disable once UnusedMember.Global -- azure functions calls this
     public static class RefreshFeedlyAuthToken
     {
         // These will be read from a settings file, or environment variables, which in production will point to the key vault.
-        private static readonly string userId = System.Environment.GetEnvironmentVariable("feedly-user-id");
-        private static readonly string refreshToken = System.Environment.GetEnvironmentVariable("feedly-refresh-token");
+        private static readonly string userId = Environment.GetEnvironmentVariable("feedly-user-id");
+        private static readonly string refreshToken = Environment.GetEnvironmentVariable("feedly-refresh-token");
 
         private const string FEEDLY_BASE_URL = "https://cloud.feedly.com/v3/";
         private const string KEY_VAULT_BASE_URL = "https://feedly-export-keyvault.vault.azure.net";
 
         [FunctionName("RefreshFeedlyAuthToken")]
+        // ReSharper disable once UnusedParameter.Global
         public static async Task Run([TimerTrigger("0 0 */6 * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
@@ -28,21 +30,21 @@ namespace FeedlyOpmlExport.Functions
 
             var kv = CreateKeyVaultClient();
 
-            log.LogInformation("Getting access token contents from keyvault");
+            log.LogInformation("Getting access token contents from key vault");
             var accessToken = await kv.GetSecretAsync(KEY_VAULT_BASE_URL, "feedly-access-token", CancellationToken.None);
             
-            var feedlyResponse = await RefreshFeedlyAccessToken(accessToken.Value, refreshToken, log);
+            var feedlyResponse = await RefreshFeedlyAccessToken(accessToken.Value, log);
 
             log.LogInformation($"TODO Remove -- new Auth token: {feedlyResponse.access_token}, plan: {feedlyResponse.plan}");
 
-            log.LogInformation("Setting the secret in the keyvault");
+            log.LogInformation("Setting the secret in the key vault");
 
             await kv.SetSecretAsync(KEY_VAULT_BASE_URL, "feedly-access-token", feedlyResponse.access_token);
 
             log.LogInformation($"Successfully updated token from {accessToken.Value} to {feedlyResponse.access_token}");
         }
 
-        private static async Task<FeedlyRefreshResponse> RefreshFeedlyAccessToken(string accessToken, string refreshToken, ILogger log)
+        private static async Task<FeedlyRefreshResponse> RefreshFeedlyAccessToken(string accessToken, ILogger log)
         {
             var client = CreateFeedlyHttpClient(accessToken);
 
@@ -81,11 +83,16 @@ namespace FeedlyOpmlExport.Functions
 
     public class FeedlyRefreshRequest
     {
+        private const string FEEDLY_CLIENT_ID = "feedlydev"; // hard-coded for users with Pro accounts
+
         // ReSharper disable UnusedMember.Global
         // ReSharper disable InconsistentNaming
+
+        // ReSharper disable once MemberCanBePrivate.Global -- used in the http request
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global -- used in http request
         public string refresh_token { get; }
-        public string client_id =>  "feedlydev";
-        public string client_secret => "feedlydev";
+        public string client_id =>  FEEDLY_CLIENT_ID;
+        public string client_secret => FEEDLY_CLIENT_ID;
         public string grant_type => "refresh_token";
         // ReSharper restore UnusedMember.Global
         // ReSharper restore InconsistentNaming
