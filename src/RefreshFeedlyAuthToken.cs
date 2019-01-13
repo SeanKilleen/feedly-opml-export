5cbdfb126d73a4514485273d857a3414af45ec9f
Sean Killeen <SeanKilleen@gmail.com>
2019-01-13 16:46:39 -0500

    Remove unused response fields

--- src/RefreshFeedlyAuthToken.cs
+++ src/RefreshFeedlyAuthToken.cs
@@ -98,10 +98,7 @@ namespace FeedlyOpmlExport.Functions
 
     public class FeedlyRefreshResponse
     {
-        public string id { get; set; }
         public string access_token { get; set; }
-        public long expires_in { get; set; }
-        public string token_type { get; set; }
         public string plan { get; set; }
 
     }
