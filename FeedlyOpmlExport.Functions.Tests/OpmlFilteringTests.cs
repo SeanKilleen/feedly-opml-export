7448f4f498ef5c80c0e1b1b45f743cf3c742cb8e
Sean Killeen <SeanKilleen@gmail.com>
2019-01-14 07:24:35 -0500

    Test project & xml file

---
+++ FeedlyOpmlExport.Functions.Tests/OpmlFilteringTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml;
+using FluentAssertions;
+using Xunit;
+
+namespace FeedlyOpmlExport.Functions.Tests
+{
+    public class OpmlFilteringTests
+    {
+        [Fact]
+        public void FiltersCorrectly()
+        {
+            var contents = File.ReadAllText("TestOpmlFile.xml");
+
+            var categories = new List<string> {"development", "tech"};
+            var result = OpmlFilterer.FilterToCategories(contents, categories);
+
+            result.Should().Contain("text=\"Gizmodo\"");
+            result.Should().NotContain("text=\"Docs Blog\"");
+        }
+    }
+}
