33333a52c47a22c3986850f659f433e7b4c91266
Sean Killeen <SeanKilleen@gmail.com>
2021-08-09 13:30:41 -0400

    Use secret client instead of keyvault client

--- src/Utilities/KeyVaultManager.cs
+++ src/Utilities/KeyVaultManager.cs
@@ -1,7 +1,7 @@
-using System.Threading;
+using System;
 using System.Threading.Tasks;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 namespace FeedlyOpmlExport.Functions
 {
@@ -10,24 +10,25 @@ namespace FeedlyOpmlExport.Functions
         private const string KEY_VAULT_BASE_URL = "https://feedly-export-keyvault.vault.azure.net";
         private const string ACCESS_TOKEN_KEY_NAME = "feedly-access-token";
 
-        private static KeyVaultClient theClient = CreateKeyVaultClient();
+        private static readonly SecretClient theSecretClient = CreateSecretClient();
 
         public static async Task<string> GetFeedlyAccessToken()
         {
-            var accessToken = await theClient.GetSecretAsync(KEY_VAULT_BASE_URL, ACCESS_TOKEN_KEY_NAME, CancellationToken.None);
+            var accessTokenResponse = await theSecretClient.GetSecretAsync(ACCESS_TOKEN_KEY_NAME);
+            var accessToken = accessTokenResponse.Value;
+            
             return accessToken.Value;
         }
-        private static KeyVaultClient CreateKeyVaultClient()
-        {
-            var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            var authenticationCallback = new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback);
 
-            return new KeyVaultClient(authenticationCallback);
+        private static SecretClient CreateSecretClient()
+        {
+            var client = new SecretClient(vaultUri: new Uri(KEY_VAULT_BASE_URL), credential: new DefaultAzureCredential());
+            return client;
         }
 
         public static async Task UpdateFeedlyAccessToken(string feedlyResponseAccessToken)
         {
-            await theClient.SetSecretAsync(KEY_VAULT_BASE_URL, ACCESS_TOKEN_KEY_NAME, feedlyResponseAccessToken);
+            await theSecretClient.SetSecretAsync(ACCESS_TOKEN_KEY_NAME, feedlyResponseAccessToken);
         }
     }
 }
